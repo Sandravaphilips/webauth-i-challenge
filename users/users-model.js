2bc23b2d6df7f4de8f13624f74f122ed15948872
Sandrava Philips <lacasandra1@gmail.com>
2019-11-11 13:41:49 +0100

    built my users model

---
+++ users/users-model.js
@@ -0,0 +1,27 @@
+const db = require('../data/db-config.js');
+
+module.exports = {
+  add,
+  find,
+  findBy,
+  findById,
+};
+
+function find() {
+  return db('users').select('id', 'username', 'password');
+}
+
+function add(user) {
+  return db('users')
+    .insert(user, 'id')
+    .then(ids => {
+      const [id] = ids;
+      return findById(id);
+    });
+}
+
+function findById(id) {
+  return db('users')
+    .where({ id })
+    .first();
+}
