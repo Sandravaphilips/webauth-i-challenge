09941978124507a44abfc35e153627656651bc33
Sandrava Philips <lacasandra1@gmail.com>
2019-11-11 13:35:48 +0100

    created a users table

--- data/migrations/20191111132938_build-users-table.js
+++ data/migrations/20191111132938_build-users-table.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('users', table => {
+        table.increments();
+        table.string('username', 50);
+        table.varchar("password");
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('users');
 };
