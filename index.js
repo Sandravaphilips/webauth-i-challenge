725a034acd47e596d8c772241c267712c9983663
Sandrava Philips <lacasandra1@gmail.com>
2019-11-11 14:00:02 +0100

    built my users server

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const UsersRoutes =  require('./users/users-router');
+
+const app = express();
+
+app.use(helmet());
+app.use(express.json());
+app.use(cors());
+
+app.use('/', UsersRoutes);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
